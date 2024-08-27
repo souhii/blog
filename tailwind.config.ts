c3ee71bd87be826a690187d66ce4eca3fd885a1d
ping.song <4739837@gmail.com>
2024-08-27 22:11:47 +0800

    :lipstick: add daisyui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss'
+import daisy from 'daisyui'
 
 export default {
   content: [
@@ -12,6 +13,6 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [daisy],
 } satisfies Config
 
